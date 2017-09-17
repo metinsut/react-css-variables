7d52e48759632f473ceb67e77982f53b1e41e990
mtnst <metindir@gmail.com>
2017-09-17 18:42:16 +0300

    edit2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+        "react/prefer-stateless-function": 0,
+        "react/jsx-filename-extension":0
+    }
 };
--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import image from './Assets/Images/img_forest.jpg';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      spacing: '10',
+      blur: '10',
+      color: '#f1c40f',
+    };
+  }
+
+  componentDidMount() {
+    // this.Input.value = this.state.spacing;
+    console.log('====================================');
+    console.log(this.img);
+    console.log('====================================');
+  }
+
+
+  changeSpaceing(e) {
+    this.setState({
+      spacing: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>Update CSS Variables</h1>
+        <div className="controls">
+          <div className="spacing" ref='a' >
+            <label htmlFor="spacing">Spacing:</label>
+            <input type="range" className="spacing" name="spacing" min="10" max="200" defaultValue={this.state.spacing} onChange={(e) => this.changeSpaceing(e)} />
+          </div>
+          <div className="blur">
+            <label htmlFor="blur">Blur:</label>
+            <input type="range" className="blur" name="blur" min="0" max="25" defaultValue={this.state.blur} />
+          </div>
+          <div className="color">
+            <label htmlFor="base">Base Color:</label>
+            <input type="color" className="color" name="base" defaultValue={this.state.color} />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="image" ref={(el) => { this.img = el; }}>
+          <img src={image} alt="forest_image" />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
